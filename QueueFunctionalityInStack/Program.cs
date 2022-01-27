893c42db856c4dbd0d0d99e6736aa112cf26927a
Kannan Subramanian <kannans1984@hotmail.com>
2022-01-27 14:07:50 +0530

    Console application to implement the Queue functionality using stack collection

---
+++ QueueFunctionalityInStack/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueFunctionalityInStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Print();
+            queue.DeQueue();
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.DeQueue();
+            queue.Enqueue(6);
+            queue.Print();
+            queue.DeQueue();
+            queue.Print();
+        }
+    }
+}
---
+++ QueueFunctionalityInStack/Queue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueFunctionalityInStack
+{
+    public class Queue
+    {
+
+        Stack<int> stack1 = new Stack<int>();
+        Stack<int> stack2 = new Stack<int>();
+
+
+        public void Enqueue(int itemToEnqueue)
+        {
+            if(stack1.Any())
+            {
+                while (stack1.Count != 0)
+                {
+                    stack2.Push(stack1.Pop());
+                }
+            }
+            //Console.WriteLine($"Enqueue :  {itemToEnqueue}");
+            stack1.Push(itemToEnqueue);
+
+            if(stack2.Any())
+            {
+                while (stack2.Count != 0)
+                {
+                    stack1.Push(stack2.Pop());
+                }
+            }
+        }
+
+        public void DeQueue()
+        {
+            //Console.WriteLine($"Dequeue :  {stack1.Peek()}");
+            stack1.Pop();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Items in stack");
+            foreach(var item in stack1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
